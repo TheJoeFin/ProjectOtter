70b2f57a21b234339496faa7c7478da279ee6649
Joseph Finney <TheJoeFin@users.noreply.github.com>
2024-01-08 22:23:37 -0600

    Add Zip Entry Item

---
+++ ProjectOtter/Models/ZipEntryItem.cs
@@ -0,0 +1,23 @@
+﻿using System.IO.Compression;
+
+namespace ProjectOtter.Models;
+
+public class ZipEntryItem
+{
+    public ZipArchiveEntry Entry { get; set; }
+
+    public string Name => Entry.FullName;
+
+    public string FirstChar => Name[0].ToString();
+
+    public bool IsJSON => Path.GetExtension(Entry.FullName) == ".json";
+
+    public string Content { get; set; } = string.Empty;
+
+    public bool IsEmpty => Entry.CompressedLength == 0;
+
+    public ZipEntryItem(ZipArchiveEntry entry)
+    {
+        Entry = entry;
+    }
+}
